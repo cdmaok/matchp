882e656cf62d8613b268616855e8146c90390a0b
cdmaok <cdmaok@126.com>
2016-05-08 15:36:14 +0800

    fix a return fixed bug

--- matchp-core/src/main/java/cn/edu/xmu/gxj/matchp/util/Fields.java
+++ matchp-core/src/main/java/cn/edu/xmu/gxj/matchp/util/Fields.java
@@ -4,5 +4,5 @@ public class Fields {
 	public static final String queryField = "text";
 	public static final String polarity = "polarity";
 	public static final String text = "text";
-	public static final String url = "text";
+	public static final String url = "img_url";
 }
