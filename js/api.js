1b8763ff500d858f3b5b342bd4d74d9814384bb3
ambeta <cyandrewchen@gmail.com>
2016-06-05 10:40:13 +0800

    new page

---
+++ js/api.js
@@ -0,0 +1,49 @@
+(function ($, window, document) {
+    var MP = window.MP || {};
+
+    $.support.cors = true;
+    
+    var baseURL = 'http://121.192.180.198:8080/matchp-web/api/';
+    var globalAPI = {
+        getImgsByText : {
+            name : 'queryImgsByText',
+            server : '',
+            method : 'POST'
+        }, 
+        getImproveDetail : {
+            name : 'queryImprove',
+            server : 'improve',
+            method : 'POST'
+        },
+        setImproveDetail : {
+            name : 'setImproveDetail',
+            server : 'improve',
+            method : 'POST'
+        }
+    };
+
+    var loadData = function (serverName, params, data) {
+        var df = $.Deferred(),
+            apiCfg = globalAPI[serverName];
+        if (!apiCfg) throw (serverName + 'not found');
+        if (data != null) {
+            df.resolve(data);
+            return df.promise();
+        }
+        $.ajax({
+            url : baseURL + (apiCfg.server || ''),
+            dataType : 'json',
+            contentType: 'application/json',
+            type : apiCfg.method || 'GET'
+        }).done(function (rsp) {
+            df.resolve(rsp);
+        }).fail(function (rsp) {
+            df.reject(rsp);
+        });
+        return df.promise();
+    };
+
+    MP.loadData = loadData;
+    window.MP = MP;
+
+})(jQuery, window, document, undefined);
---
+++ js/page.improve.js
@@ -0,0 +1,160 @@
+(function ($, window, document, undefined) {
+  var MP = window.MP || {};
+
+  var testData = {
+    "query" : "一天的工作又开始了，忙并快乐着[偷笑]",
+    "entrys" : [{
+      "text" : "我闻到了，黑木耳的味道",
+      "img" : "http://ww2.sinaimg.cn/large/491fdd54jw1e8zwjkqwcvj218g0xc123.jpg" 
+    },{
+      "text" : "红宝石，蓝宝石，啊，一碰就碎",
+      "img" : "http://xinlingzu.lofter.com/post/1d33aff_b08b6d1"
+    }]
+  };
+
+  var ImprovePage = function (cfg) {
+    this.container = (cfg && cfg.container) || $('body');
+
+    this.data = null;
+    this.$body = null;
+    this.$imgBox = null;
+
+    this.renderLayout();
+    this.bindEvents();
+    this.fetchData();
+  };
+
+  ImprovePage.prototype = {
+
+    fetchData : function () {
+      var self = this,
+          params = {};
+      MP.loadData('getImproveDetail', params/*, testData*/)
+        .done(function (rsp) {
+          self.data = rsp;
+          self.render();
+        })
+        .fail(function (rsp) {
+          alert('Fail to fetch data from server, please try again later.');
+        })
+        .always(function () {
+
+        });
+    },
+
+    sendData : function (params) {
+      var self = this;
+      MP.loadData('getImproveDetail', params/*, testData*/)
+        .done(function () {
+          self.data = null;
+          self.fetchData();
+        })
+        .fail(function (rsp) {
+          alert('Fail to send data to server, please try again later.');
+        });
+    },
+
+    mapLayoutRenderData : function () {
+      return {
+        title : 'Which One Is BETTER ?'
+      };
+    },
+
+    renderLayout : function () {
+      var self = this,
+          renderData = self.mapLayoutRenderData(),
+          htm = self.genLayoutHtml(renderData);
+      self.container.html(htm);
+      self.$body = self.container.find('.mp-ip-body');
+      self.$imgBox = self.container.find('.ip-image-box');
+    },
+
+    mapRenderData : function () {
+      var self = this, data = self.data;
+      return {
+        text : data.query,
+        images : $.map(data.entrys, function (val, idx) {
+          return {
+            src : val.img || '',
+            title : val.text || '',
+            answer : idx == 0 ? 1 : -1
+          };
+        })
+      };
+    },
+
+    render : function () {
+      var self = this,
+          renderData = self.mapRenderData(),
+          htm = self.genImgBoxHtml(renderData);
+      self.$imgBox.html(htm);
+    },
+
+    bindEvents : function () {
+      var self = this;
+      self.$imgBox
+        .on('click', '.img-wrapper a', function () {
+          var $this = $(this),
+              data = self.data;
+          data.answer = $this.data('answer');
+          self.sendData(data);
+        })
+        .on('imgerror', function (evt) {
+          var ele = evt.target;
+          ele.src = './imgs/default.png';
+          ele.onerror = null;
+        })
+    },
+
+    genLayoutHtml : function (ctx) {
+      var self = this,
+          title = ctx.title || '';
+      return (
+        '<header><h1>' + title + '</h1></header>' +
+        '<div class="container mp-ip-body">' + 
+          '<div class="alert alert-warning text-center">' + 
+            'Thanks for helping us to improve the accuracy of matching. ' +
+            'Please click on the image which you feel better fitting the text.' + 
+          '</div>' + 
+          '<div class="row well ip-image-box"></div>' + 
+        '</div>'
+      );
+    },
+
+    genImgBoxHtml : function (ctx) {
+      var self = this,
+          text = ctx.text || '',
+          images = ctx.images || [],
+          imagesHtml = $.map(images, function (val) {
+            return self.genImgHtml(val);
+          }).join('');
+      return (
+        '<div class="col-sm-6 col-sm-offset-3 text-center">' + 
+          '<p class="query-text" >' + text + '</p>' +
+        '</div>' + 
+        imagesHtml
+      );
+    },
+
+    genImgHtml : function (ctx) {
+      var clz = ctx.clz || 'col-sm-6',
+          answer = ctx.answer || 1,
+          src = ctx.src || '',
+          title = ctx.title || 'PIC',
+          alt = ctx.alt || title;
+      return (
+        '<div class="' + clz + '">' +
+          '<div class="img-wrapper">' + 
+            '<a href="javascript:void(0);" data-answer="' + answer + '">' + 
+              '<img class="img-content" src="' + src + '" title="' + title + '" alt="' + alt + '" onerror="jQuery(this).trigger(\'imgerror\', (arguments[0]));" />' +
+            '</a>' +
+          '</div>' +
+        '</div>'
+      );
+    }
+  };
+
+  MP.ImprovePage = ImprovePage;
+  window.MP = MP;
+
+})(jQuery, window, document);
